c444972cb7bd0421cf60f20781c734bf59f3d94a
Will Harborne <will.harborne@googlemail.com>
2019-09-12 10:11:01 +0100

    Create endpoint to return feeRate
    - Preparation for new fee schedule changes

---
+++ src/models/methods/calculateFeeForAddress.js
@@ -0,0 +1,68 @@
+const BigNumber = require('bignumber.js')
+const _ = require('lodash')
+const moment = require('moment')
+
+const calculateVolumeForAddress = require('../../models/methods/calculateVolumeForAddress')
+
+module.exports = async (address, endDate = (moment().valueOf() / 1000)) => {
+
+  // start date is 30 days ago
+  const startDate = moment(endDate * 1000).subtract(30, 'days').valueOf() / 1000
+  // const volume = await calculateVolumeForAddress(address, startDate, endDate)
+  const volume = {totalUSDValue: 1000000000000000}
+  let total = volume.TotalUSDValue
+
+  if(!total) {
+    total = 0
+  }
+
+  let volumeDiscount
+
+  switch (true) {
+    case (total < 150000):
+      volumeDiscount = 0
+      break;
+    case (total < 1000000):
+      volumeDiscount = 0.05
+      break;
+    case (total < 4000000):
+      volumeDiscount = 0.10
+      break;
+    case (total < 9000000):
+      volumeDiscount = 0.15
+      break;
+    case (total < 18000000):
+      volumeDiscount = 0.20
+      break;
+    case (total < 30000000):
+      volumeDiscount = 0.25
+      break;
+    case (total >= 30000000):
+      volumeDiscount = 0.30
+      break;
+  }
+
+  const baseFeeBps = new BigNumber(25)
+  const accountFee = [
+    baseFeeBps.times(1 - volumeDiscount).toFixed(0),
+    baseFeeBps.times(1 - volumeDiscount).times(0.85).toFixed(0),
+    baseFeeBps.times(1 - volumeDiscount).times(0.8).toFixed(0),
+  ]
+
+  const response = {
+    small: {
+      threshold: 0,
+      feeBps: accountFee[0]
+    },
+    medium: {
+      threshold: 500,
+      feeBps: accountFee[1]
+    },
+    large: {
+      threshold: 2000,
+      feeBps: accountFee[2]
+    }
+  }
+
+  return response
+}
---
+++ src/routes/api/v1/feeRate.js
@@ -0,0 +1,34 @@
+const { param } = require('express-validator')
+const validatorsAreMet = require('../../../lib/http/validatorsAreMet')
+
+const cacheFunction = require('../../../lib/cache/cacheFunction')
+
+const calculateVolumeForAddress = require('../../../models/methods/calculateFeeForAddress')
+
+/**
+ * Return current fee rate for a given address
+ */
+module.exports = (server) => {
+  server.get('/api/v1/feeRate/:address', [
+    param('address').isString(),
+
+    validatorsAreMet
+  ], async (req, res) => {
+
+    const address = req.params.address
+
+    // key we will use to cache this calculation
+    const cacheKey = 'feeRate:' + address
+    // cache calculation for 1 Hour
+    const cacheTime = 60 * 60
+
+    const result = await cacheFunction(cacheKey, cacheTime, async () => {
+      const result = await calculateFeeForAddress(address)
+
+      return result
+    })
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(result || {error: 'not_found'})
+  })
+}
